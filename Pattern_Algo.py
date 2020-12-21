d9481ff51ddd9651295692757052c10c8d24d9f6
SATYAM GUPTA <73251754+SatyamJS@users.noreply.github.com>
2020-12-21 23:55:22 +0530

    Update Pattern_Algo.py

--- Pattern_Algo.py
+++ Pattern_Algo.py
@@ -1,9 +1,13 @@
+import time
 n,m=map(int,input().split())
 w="WELCOME"
 for i in range(1,(n//2)+1):
+    time.sleep(1)
     print("-"*((m-3*(2*i-1))//2)+".|."*(2*i-1)+"-"*((m-3*(2*i-1))//2))
+time.sleep(1)
 print("-"*((m-len(w))//2)+w+"-"*((m-len(w))//2))
 j=n//2
 for i in range(1,(n//2)+1):
+    time.sleep(1)
     print("-"*((m-3*(2*j-1))//2)+".|."*(2*j-1)+"-"*((m-3*(2*j-1))//2))
     j-=1
